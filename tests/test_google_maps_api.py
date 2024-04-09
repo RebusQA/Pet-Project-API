310cb8a9f553c898aaca63b95fe914b7634a71d9
RebusQA <rebusqae@gmail.com>
2024-04-09 09:22:20 +0300

    fixing variable

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -8,9 +8,9 @@ class Test_create_place():
     def test_create_new_place(self):
 
         print("Метод POST")
-        result_post: Response = Google_maps_api.create_new_place()
+        result_post = Google_maps_api.create_new_place()    # Переменная, хранит экземпляр класса из которой вызываю необходимый метод.
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
         print("Метод GET")
-        result_get: Response = Google_maps_api.get_new_place(place_id)
+        result_get = Google_maps_api.get_new_place(place_id)
