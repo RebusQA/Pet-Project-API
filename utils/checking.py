530ab87cacd8df3c13dfecea4902572201ba374a
RebusQA <rebusqae@gmail.com>
2024-04-09 13:18:03 +0300

    Checking status code. PyTest 5

---
+++ utils/checking.py
@@ -0,0 +1,14 @@
+
+"""Методы для проверки ответов запросов"""
+from requests import Response
+
+class Checking():
+
+    """Метод для проверки статус кода"""
+    @staticmethod
+    def check_status_code(response: Response, status_code):
+        assert  status_code == response.status_code
+        if response.status_code == status_code:
+            print("Успешно. Статус код = " + str(response.status_code))
+        else:
+            print("Провал. Статус код = " + str(response.status_code))
