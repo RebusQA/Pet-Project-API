eb5604fdd20916a781318a10035d6a3cce46dd37
RebusQA <rebusqae@gmail.com>
2024-04-09 14:30:25 +0300

    Checking required fields. PyTest 6

--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,3 +1,4 @@
+import json
 
 from utils.api import Google_maps_api
 from utils.checking import Checking
@@ -10,26 +11,38 @@ class Test_create_place():
         print("\nМетод POST")
         result_post = Google_maps_api.create_new_place()    # Переменная, хранит экземпляр класса из которой вызываю необходимый метод.
         check_post = result_post.json()
-
         place_id = check_post.get("place_id")
         Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        token = json.loads(result_post.text)
+        print(list(token))
 
         print("Метод GET POST")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ["location", "accuracy", "name", "phone_number",
+                                               "address","types", "website", "language"])
 
         print("Метод PUT")
         result_put = Google_maps_api.put_new_place(place_id)
         Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ["msg"])
 
         print("Метод GET PUT")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get,
+                                  ["location", "accuracy", "name", "phone_number",
+                                   "address", "types", "website", "language"])
 
         print("Метод DELETE")
         result_delete = Google_maps_api.delete_new_place(place_id)
         Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ["status"])
 
         print("Метод GET DELETE")
         result_get = Google_maps_api.get_new_place(place_id)
         Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ["msg"])
+
+        print("\nТестирование создание, изменение и удаление новой локации, прошло успешно.")
--- utils/checking.py
+++ utils/checking.py
@@ -1,5 +1,6 @@
 
 """Методы для проверки ответов запросов"""
+import json
 from requests import Response
 
 class Checking():
@@ -12,3 +13,11 @@ class Checking():
             print("Успешно. Статус код = " + str(response.status_code))
         else:
             print("Провал. Статус код = " + str(response.status_code))
+
+
+    """Метод для проверки наличия обязательных полей в ответе запроса"""
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token) == expected_value
+        print("Все поля присутствуют")
